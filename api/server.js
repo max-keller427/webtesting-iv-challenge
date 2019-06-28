c0f0ff005c14fb7ee769f81bcad1aca5a462be95
Max Keller <max.keller427@gmail.com>
2019-06-27 22:40:34 -0600

    mvp

--- api/server.js
+++ api/server.js
@@ -10,4 +10,43 @@ server.get("/", (req, res) => {
   res.status(200).json({ message: "he" });
 });
 
+let roomies = [
+  {
+    id: 1,
+    name: "Max"
+  }
+];
+
+let nextId = 2;
+
+server.post("/roomies", (req, res) => {
+  //   console.log(req.body);
+  if (req.body.name) {
+    roomies.push({ id: nextId, name: req.body.name });
+    nextId++;
+    res.status(201).json(roomies);
+  } else {
+    res.status(500).json({ message: "please provide a name and id" });
+  }
+});
+
+server.delete("/roomies/:id", (req, res) => {
+  let roomieExists = false;
+  let roomieIndex = null;
+
+  roomies.forEach((roomie, i) => {
+    if (roomie.id === parseInt(req.params.id)) {
+      roomieExists = true;
+      roomieIndex = i;
+    }
+  });
+
+  if (roomieExists) {
+    roomies.splice(roomieIndex, 1);
+    res.status(200).json({ message: "deleted" });
+  } else {
+    res.status(404).json({ message: "that roomie got kicked out" });
+  }
+});
+
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -12,4 +12,34 @@ describe("server.js", () => {
         });
     });
   });
+  describe("the post", () => {
+    it("should return 201 on a post", async () => {
+      const res = await request(server)
+        .post("/roomies")
+        .send({ name: "Alex" });
+
+      expect(res.status).toBe(201);
+    });
+    it("should return 500 on a post without a name", async () => {
+      const res = await request(server)
+        .post("/roomies")
+        .send("Dilly");
+
+      expect(res.status).toBe(500);
+    });
+
+    describe("the delete", () => {
+      it("sends status 200 if roomie exists", async () => {
+        const res = await request(server).delete("/roomies/1");
+
+        expect(res.status).toBe(200);
+      });
+
+      it("sends status 404 if roomie does not exist", async () => {
+        const res = await request(server).delete("/roomies/1");
+
+        expect(res.status).toBe(404);
+      });
+    });
+  });
 });
